8a739c6d4a210a6b44878de02f18f07d58782ede
Delta <pajchima.ch@gmail.com>
2026-07-07 13:41:10 +0700

    add chrome-burger-db

--- query.mongodb.js
+++
@@ -1,7 +0,0 @@
-use("sample_mflix");
-
-db.comments.find();
-
-db.comments.findOne({ email: “john_bishop@fakegmail.com”});
-
-db.comments.findOne({ name: “John Bishop”});
--- query3.mongodb.js
+++
@@ -1,9 +0,0 @@
-use("sample_mflix");
-
-db.theaters.find({ "location.address.state": "AL" }).count();
-
-db.theaters.find({ "location.address.state": "La Quinta"}).count();
-
-db.theaters.findOne({ "location.address.state": "AL" })
-
-db.theaters.findOne({ "location.address.state": "La Quinta"})
